4b1f889878e564ac064a9c86d366e08089594deb
github username <github email address>
2017-12-17 15:15:56 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,63 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {cats: [], fishes: [], dogs: []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.length
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def buy_cat(name)
+    self.pets[:cats] << Cat.new(name)
+  end
+
+  def buy_fish(name)
+    self.pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_dog(name)
+    self.pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    self.pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    self.pets[:fishes].each {|fish| fish.mood = "happy"}
+  end
+
+  def sell_pets
+    self.pets.each do |species, animals|
+      animals.each { |pet| pet.mood = "nervous"}
+    end
+    self.pets.clear
+  end
+
+  def list_pets
+    "I have #{self.pets[:fishes].length} fish, #{self.pets[:dogs].length} dog(s), and #{self.pets[:cats].length} cat(s)."
+  end
 end
